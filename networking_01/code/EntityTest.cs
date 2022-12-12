14e73ed675756765c936f9baf682af7adc8de3fd
Jake Evans <jacob.ee92@gmail.com>
2022-12-11 19:45:17 -0600

    Networking basics

---
+++ networking_01/code/EntityTest.cs
@@ -0,0 +1,56 @@
+﻿
+using Sandbox;
+
+public partial class EntityTest : Entity
+{
+
+	[Net]
+	public int NetworkedInt { get; set; }
+	[Net]
+	public string NetworkedString { get; set; }
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		// Server-side initialization
+
+		NetworkedInt = 55;
+	}
+
+	public override void ClientSpawn()
+	{
+		base.ClientSpawn();
+
+		// Client-side initialization
+	}
+
+	[ClientRpc]
+	public void ShowDeathMessage(string deathMessage )
+	{
+		if ( Game.IsServer )
+		{
+			Log.Error( "THIS SHOULDN'T CALL ON THE SERVER!" );
+			return;
+		}
+
+		Log.Error( deathMessage );
+	}
+
+	[Event.Tick]
+	public void OnTick()
+	{
+		var lineNumber = Game.IsClient ? 0 : 1;
+		var color = Game.IsClient ? Color.Green : Color.Yellow;
+		DebugOverlay.ScreenText( "NetworkedInt value is: " + NetworkedInt, new Vector2( 64, 64 ), lineNumber, color );
+
+		if ( Game.IsServer )
+		{
+			ShowDeathMessage( "You died" );
+
+			ShowDeathMessage( To.Everyone, "EVeryone died" );
+			ShowDeathMessage( To.Single( this ), "You died" );
+		}
+	}
+
+}
---
+++ networking_01/code/Game.cs
@@ -0,0 +1,33 @@
+﻿
+using Sandbox;
+
+public partial class MyGame : GameManager
+{
+
+	[Net]
+	public EntityTest TestingEntity { get; set; }
+
+	public MyGame()
+	{
+		if ( Game.IsServer )
+		{
+			TestingEntity = new EntityTest();
+		}
+	}
+
+	public override void ClientJoined( IClient cl )
+	{
+		base.ClientJoined( cl );
+
+		// cl.Name has joined the game
+		// cl.Pawn = new Entity()...
+	}
+
+	public override void ClientDisconnect( IClient cl, NetworkDisconnectionReason reason )
+	{
+		base.ClientDisconnect( cl, reason );
+
+		//cl.Name has left the game
+	}
+
+}
