86d1758d5c0e819587951bf4579ea23a78653295
Jake Evans <jacob.ee92@gmail.com>
2022-12-25 18:14:49 -0600

    Healthbar tutorial

---
+++ healthbar/code/Game.cs
@@ -0,0 +1,21 @@
+﻿
+using Sandbox;
+
+public partial class MyGame : GameManager
+{
+	public MyGame()
+	{
+		if ( Game.IsClient )
+		{
+			_ = new Hud();
+		}
+	}
+
+	public override void ClientJoined( IClient cl )
+	{
+		base.ClientJoined( cl );
+
+		cl.Pawn = new Pawn();
+	}
+
+}
